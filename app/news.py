cc43aa9530b129169289e6afa969096b1cceb8b5
jazzyk <jkirbyy@icloud.com>
2022-01-27 04:09:14 +0000

    Removed print statements

--- app/news.py
+++ app/news.py
@@ -4,16 +4,14 @@ from datetime import datetime, timedelta
 from flask import render_template, redirect, session
 
 @app.route('/', methods=['GET'])
-def get_topics():
-
+def get_topics(): #get user topics
     url = ('https://topic-builder.herokuapp.com/topic')
     topics = requests.get(url)
-    print(topics.json())
     return render_template('dropdown.html', allTopics=topics.json())
 
 @app.route('/news/<key_term>/<topic_id>', methods=['GET'])
 def get_news(key_term, topic_id):
-
+    
     date_formatted = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d") #Get yesterdays date
 
     if (len(key_term.split('%20')) > 1): #if multi word term then prepare as such
@@ -28,24 +26,10 @@ def get_news(key_term, topic_id):
     url = ('http://newsapi.org/v2/everything?'+ query + '&from=' + date_formatted + '&sortBy=popularity' + '&apiKey=9118e3daa3884321ae89c75d7e5ade07')
     news = requests.get(url)
 
-    print('topic: ' + session['topic_id'])
-    #for key in session.keys():
-        #session.pop(key)
-    #session.clear()
-    #count = 0
-    #for item in news.json()["articles"]:
-        #session[str(count)] = item["title"] + " " + item["description"]
-        #print(session[count])
-        #count += 1
     return render_template('news.html', articles=news.json()["articles"])
-   #print(news.json()["articles"])
 
 @app.route('/addterm/<key_term>', methods=['GET'])
-def add_term(key_term):
+def add_term(key_term): #add key term to topic
     url = ('https://topic-builder.herokuapp.com/createKeyterm?name=' + key_term + '&topic_id=' + session['topic_id'])
-    print(url)
     response = requests.post(url)
-    print('response: ')
-    print(response)
     return redirect('/')
-
